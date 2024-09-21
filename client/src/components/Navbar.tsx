7a4dbed6ba3e7d0f10c8a3aca2459de821c19615
Yash <yashchaurasia630@gmail.com>
2024-09-21 11:58:49 +0530

    navbar done

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,36 +5,42 @@ import { FaArrowDown, FaQuestion, FaSearch, FaSpotify } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 
 const Navbar = () => {
-  const [pages, setPages] = useState({
+  const [isHovered, setIsHovered] = useState(false);
+
+  const pages = {
     search: (
       <div className="flex gap-x-4 items-center">
         <FaSearch size={25} className="" />
-        <p className="text-xl">Search</p>
+        {isHovered ? <p className="text-xl">Search</p> : ""}
       </div>
     ),
     downloads: (
       <div className="flex gap-x-4 items-center">
         <FaArrowDown size={25} className="" />
-        <p className="text-xl">Downloads</p>
+        {isHovered ? <p className="text-xl">Downloads</p> : ""}
       </div>
     ),
     login: (
       <div className="flex gap-x-4 items-center">
         <CgProfile size={25} className="" />
-        <p className="text-xl">Login</p>
+        {isHovered ? <p className="text-xl">Login</p> : ""}
       </div>
     ),
     help: (
       <div className="flex gap-x-4 items-center">
         <FaQuestion size={25} className="" />
-        <p className="text-xl">Help</p>
+        {isHovered ? <p className="text-xl">Help</p> : ""}
       </div>
     ),
-  });
+  };
 
   return (
-    <div className="pl-4 pr-4 relative my-3 rounded-xl bg-[#242424] flex items-center w-[90px]">
-      <div className="flex flex-col gap-y-4 overflow-hidden">
+    <div
+      className="pl-4 pr-4 relative my-3 rounded-xl bg-[#242424] flex items-center w-[90px] hover:w-[200px] transition-all overflow-hidden"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="flex flex-col gap-y-4 ">
         <FaSpotify size={60} className="absolute top-6 left-[13px]" />
         <NavLink to={"/"} className={"navlink"}>
           {pages.search}
