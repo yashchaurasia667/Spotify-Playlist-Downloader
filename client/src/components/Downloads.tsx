855ab422ab7075eaeb732e5dc6d0706587295ec2
Yash <yashchaurasia630@gmail.com>
2024-09-22 11:45:53 +0530

    download page with tiles

--- client/src/components/Downloads.tsx
+++ client/src/components/Downloads.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
+import Tile from "./Tile.js";
 
 const Downloads = () => {
-  const nameClass =
-    "bg-[#242424] w-[90%] md:w-[60%] xl:w-[40%] px-8 py-3 rounded-full outline-none border-2 border-[#acacac] hover:border-white focus:border-white focus:border-[3px] transition-all";
-  const [name, setName] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -20,27 +17,11 @@ const Downloads = () => {
     }
   };
   return (
-    <div className="p-5 flex flex-col justify-center gap-y-6 mt-32">
-      <form
-        method="post"
-        className="flex lg:flex-row lg:w-auto w-[100%] flex-col justify-center items-center gap-x-3 gap-y-6"
-      >
-        <input
-          type="text"
-          name="link"
-          className={nameClass}
-          placeholder="Song name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="font-semibold w-[90%] md:w-[30%] lg:w-auto bg-purple-400 text-[#121212] rounded-full px-10 py-4 hover:scale-105 hover:bg-purple-300 transition-all"
-          onClick={handleSubmit}
-        >
-          Search
-        </button>
-      </form>
+    <div className="px-4 py-10">
+      <h1 className="font-bold text-4xl mb-4 text-purple-500">Downloads</h1>
+      <div className="mt-20">
+        <Tile title="Test Tile" coverPath="/vite.svg" />
+      </div>
     </div>
   );
 };
---
+++ client/src/components/Tile.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { FaFolder } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+
+const Tile = ({
+  title = "Enter title",
+  downloadPath = "path to download",
+  coverPath = "path to album cover",
+}) => {
+  return (
+    <div>
+      <div className="h-[100px] rounded-lg bg-[#242424] px-6 py-4 flex justify-between items-center">
+        <div className="flex gap-x-8">
+          <img src={coverPath} alt="cover" className="rounded-lg" />
+          <p className="text-lg font-semibold">{title}</p>
+        </div>
+        <div className="flex gap-x-6">
+          <button>
+            <FaFolder size={25} className="text-purple-200" />
+          </button>
+          <button>
+            <RxCross2 size={25} className="text-red-400" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tile;
